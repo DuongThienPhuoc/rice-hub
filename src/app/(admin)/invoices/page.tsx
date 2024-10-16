800435206bb5692492ba35361b1ebfa1866e3e1a
TLupus131 <hahacondien@gmail.com>
2024-10-16 11:28:28 +0700

    demo invoice template

---
+++ src/app/(admin)/invoices/page.tsx
@@ -0,0 +1,12 @@
+import Invoice from '@/components/ui/invoice';
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>
+            <Invoice />
+        </div>
+    );
+};
+
+export default Page;
