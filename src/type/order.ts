fc1d7bbd8b1e91c2e8a3799db80e19c047e32534
DuongThienPhuoc <phuocvip2@gmail.com>
2024-11-16 13:30:59 +0700

    add transaction for order detail

--- src/type/order.ts
+++ src/type/order.ts
@@ -23,6 +23,26 @@ export interface Order {
     remainingAmount: number;
     status:          string;
     orderDetails:    OrderDetail[];
+    receiptVoucher:  ReceiptVoucher;
+}
+
+export interface ReceiptVoucher {
+    id:           number;
+    receiptCode:  string;
+    receiptDate:  Date;
+    dueDate:      null;
+    totalAmount:  number;
+    paidAmount:   number;
+    remainAmount: number;
+    transactions: Transaction[];
+}
+
+export interface Transaction {
+    id:              number;
+    amount:          number;
+    transactionDate: Date;
+    paymentMethod:   string;
+    status:          string;
 }
 
 export interface Embedded {
